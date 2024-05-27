79a30c65b8c6bac17a145416d22ff5865f39c6ab
Danila Muravyev <mixidwork@gmail.com>
2024-05-27 16:35:10 +0300

    fix imports

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,8 @@
 from selenium.webdriver.common.by import By
-from .locators import MainPageLocators
-from .login_page import LoginPage
+from pages.locators import MainPageLocators
+from pages.login_page import LoginPage
 
-from .base_page import BasePage
+from pages.base_page import BasePage
 
 
 class MainPage(BasePage):
