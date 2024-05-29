59596f6197b96526abe1f1c8d5202c5a86038906
Danila Muravyev <mixidwork@gmail.com>
2024-05-29 17:49:43 +0300

    added need_review marks

--- test_product_page.py
+++ test_product_page.py
@@ -4,6 +4,7 @@ import pytest
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
 
 f = faker.Faker()
 
@@ -28,6 +29,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_succes_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
@@ -36,6 +38,7 @@ class TestUserAddToBasketFromProductPage:
         page.test_add_product_to_cart_verify()
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize(
     "promo_offer",
     [
@@ -50,6 +53,17 @@ def test_guest_can_add_product_to_basket(browser, promo_offer):
     page.test_add_product_to_cart()
     page.solve_quiz_and_get_code()
     page.test_add_product_to_cart_verify()
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/hacking-exposed-wireless_208/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_text()
 
 
 @pytest.mark.xfail()
@@ -84,6 +98,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
