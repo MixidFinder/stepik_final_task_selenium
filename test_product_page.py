b54927199c5a5e5ddcba29fa1032f7a83d2dc353
Danila Muravyev <mixidwork@gmail.com>
2024-05-29 17:07:10 +0300

    Added user tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,39 @@
+import faker
 import pytest
 
-from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
+from pages.main_page import MainPage
 from pages.product_page import ProductPage
+
+f = faker.Faker()
+
+
+@pytest.mark.user_add_product
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+        login_page.register_new_user(f.email(), f.password())
+        login_page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_succes_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.test_add_product_to_cart()
+        page.test_add_product_to_cart_verify()
 
 
 @pytest.mark.parametrize(
@@ -57,12 +89,3 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
-
-
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-age-of-the-pussyfoot_89/"
-    page = BasketPage(browser, link)
-    page.open()
-    page.go_to_basket_page()
-    page.should_be_empty_basket()
-    page.should_be_empty_basket_text()
